b28c22e12f5defc88e9f2cdb98a1fcac0b39b24a
Cristian Elvis <79227065+cristianelvis@users.noreply.github.com>
2021-05-19 14:33:39 -0300

    Add files via upload

--- freedomee/social/assets/js/library.js
+++ freedomee/social/assets/js/library.js
@@ -1,5 +1,7 @@
 $(function(){
-    $("#includeNav").load("assets/html/nav.html");
-    $("#includeIndexContent").load("assets/html/indexContent.html");
+    $("#includeNav").load("assets/html-freedomee/nav.html");
+    $("#includeMain").load("assets/html-freedomee/main.html");
+    $("#includeExtra").load("assets/html-freedomee/extra.html");
+    $("#includeIndexContent").load("assets/html-freedomee/indexContent.html");
 
 });
