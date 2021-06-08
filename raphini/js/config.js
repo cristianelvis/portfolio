b62c51efeec011d928637bcb6e08ac9144e44c8f
Cristian Elvis <79227065+cristianelvis@users.noreply.github.com>
2021-06-07 21:10:03 -0300

    Add files via upload

--- raphini/js/config.js
+++ raphini/js/config.js
@@ -73,7 +73,7 @@ $(function() {
 	simpleStore.init({
 
 		// brand can be text or image URL
-		brand : "Dimensão",
+		brand : "Raphini",
 
 		// numder of products per row (accepts 1, 2 or 3)
 		numColumns : 3,
