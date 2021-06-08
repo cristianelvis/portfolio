5b988b16701f497907c7247072b5560cd5574dac
Cristian Elvis <79227065+cristianelvis@users.noreply.github.com>
2021-06-07 21:38:05 -0300

    Add files via upload

--- raphini/js/config.js
+++ raphini/js/config.js
@@ -51,7 +51,7 @@ $(function() {
 	    shippingTotalRate: 0,
 
 	    // tax rate applied to cart subtotal
-	    taxRate: 0.1,
+	    taxRate: 01.50,
 
 	    // true if tax should be applied to shipping
 	    taxShipping: false,
