8b323816d755ce67a6d257c79873daeae66c995e
Cristian Elvis <79227065+cristianelvis@users.noreply.github.com>
2021-08-11 01:00:21 -0300

    Add files via upload

--- freedomee/landing/assets/js/scripts.js
+++ freedomee/landing/assets/js/scripts.js
@@ -1,7 +1,3 @@
-$(document).ready(function(){
-    $("#menu").load("content/menu.html");
-    $("#section001").load("content/section001.html");
-});
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll >= 100) {
@@ -9,4 +5,21 @@ $(window).scroll(function() {
     } else {
         $(".navHide").removeClass("navShow");
     }
+});
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+$(document).ready(function(){
+    $("#menu").load("content/menu.html");
+    $("#section001").load("content/section001.html");
 });
