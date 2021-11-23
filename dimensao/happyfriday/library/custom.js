b43f4f2e1f532097065f4d96a00ed2ffa4ce1c9f
Cristian Elvis <79227065+cristianelvis@users.noreply.github.com>
2021-11-23 11:21:22 -0300

    Add files via upload

---
+++ dimensao/happyfriday/library/custom.js
@@ -0,0 +1,47 @@
+// Movimenta os objetos no banner
+var object01=$('#object01');
+var object02=$('#object02');
+var object03=$('#object03');
+var object04=$('#object04');
+
+var layer1=$('#layer1');
+layer1.mousemove(function(e){
+	var valueX=(e.pageX * -1 / 15); 
+	var valueY=(e.pageY * -1 / 15); 
+    object01.css({
+        'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
+    });
+});
+layer1.mousemove(function(e){
+	var valueX=(e.pageX * -1 / 20); 
+	var valueY=(e.pageY * -1 / 20); 
+    object02.css({
+        'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
+    });
+});
+layer1.mousemove(function(e){
+   var valueX=(e.pageX * -1 / 20); 
+   var valueY=(e.pageY * -1 / 20); 
+    object03.css({
+        'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
+    });
+});
+layer1.mousemove(function(e){
+	var valueX=(e.pageX * -1 / 15); 
+	var valueY=(e.pageY * -1 / 15); 
+    object04.css({
+        'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
+    });
+});
+
+// mostra o logo somente quando roalr a página
+window.onscroll = function() {
+    var doc = document.documentElement;
+    var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+    if(top <= 80) {
+       jQuery(".showlogo").css('display', 'none');
+    }
+    else {
+       jQuery(".showlogo").css('display', 'unset');
+    }
+}
