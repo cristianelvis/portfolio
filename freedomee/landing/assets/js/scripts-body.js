941ef8e5619b2834eebd878db2eb8abf3e3edc96
Cristian Elvis <79227065+cristianelvis@users.noreply.github.com>
2022-01-30 19:57:33 -0300

    Add files via upload

--- freedomee/landing/assets/js/scripts-body.js
+++ freedomee/landing/assets/js/scripts-body.js
@@ -1,2 +1,8 @@
-
+function showFlex(el) {
+    var display = document.getElementById(el).style.display;
+        if (display == "none")
+            document.getElementById(el).style.display = 'flex';
+        else
+            document.getElementById(el).style.display = 'none';
+}
 
