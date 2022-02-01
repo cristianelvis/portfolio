4a99721b40f35467bafbd5f3a4e0dbbea9eeefb0
Cristian Elvis <79227065+cristianelvis@users.noreply.github.com>
2022-02-01 12:34:31 -0300

    Add files via upload

---
+++ freedomee/landing/assets/js/scripts-body.js
@@ -0,0 +1,140 @@
+function showEconomicContent() {
+    var x = document.getElementById("economicContent1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    };
+    var x = document.getElementById("economicContent2");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+function showHumanContent() {
+    var x = document.getElementById("humanContent1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    };
+    var x = document.getElementById("humanContent2");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+function showAnthropicContent() {
+    var x = document.getElementById("anthropicContent1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    };
+    var x = document.getElementById("anthropicContent2");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+function showTechnologyContent() {
+    var x = document.getElementById("technologyContent1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    };
+    var x = document.getElementById("technologyContent2");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+function showQualityContent() {
+    var x = document.getElementById("qualityContent1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    };
+    var x = document.getElementById("qualityContent2");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+function showValueOneContent() {
+    var x = document.getElementById("valueOneContent1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    };
+    var x = document.getElementById("valueOneContent2");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+function showValueTwoContent() {
+    var x = document.getElementById("valueTwoContent1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    };
+    var x = document.getElementById("valueTwoContent2");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+function showValueThreeContent() {
+    var x = document.getElementById("valueThreeContent1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    };
+    var x = document.getElementById("valueThreeContent2");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+function showValueFourContent() {
+    var x = document.getElementById("valueFourContent1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    };
+    var x = document.getElementById("valueFourContent2");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+function showValueFiveContent() {
+    var x = document.getElementById("valueFiveContent1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    };
+    var x = document.getElementById("valueFiveContent2");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
---
+++ freedomee/landing/assets/js/scripts.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $("#navtop").load("../br/_nav-top.html");
+    $("#navbottom").load("../br/_nav-bottom.html");
+});
