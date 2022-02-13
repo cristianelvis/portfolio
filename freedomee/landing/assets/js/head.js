c50dfa94b269a10673104eb245d8912894564f31
Cristian Elvis <79227065+cristianelvis@users.noreply.github.com>
2022-02-13 19:07:09 -0300

    Add files via upload

---
+++ freedomee/landing/assets/js/head.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#navtop").load("../inicio/_nav-top.html");
+    $("#navbottom").load("../inicio/_nav-bottom.html");
+    $("#modal").load("../inicio/_modal.html");
+});
