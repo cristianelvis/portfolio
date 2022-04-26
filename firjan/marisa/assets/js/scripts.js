e13eeae9d0437b436c180448bbb52b1645095383
Cristian Elvis <79227065+cristianelvis@users.noreply.github.com>
2022-04-26 18:11:13 -0300

    Add files via upload

---
+++ firjan/marisa/assets/js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $("#head").load("../dashboard/_include_head.html");
+    $("#footer").load("../dashboard/_include_footer.html");
+    $("#modal").load("../dashboard/_include_modal.html");
+    $("#dashboard").load("section_001.html");
+    $("#gestaodepessoas").load("section_001.html");
+    $("#mrhDesligamento").load("section_001.html");
+    $("#mrhAdmissao").load("section_001.html");
+    $("#mrhAlteracaoCargaHoraria").load("section_001.html");
+    $("#mrhAlteracaoCargoEspecialidade").load("section_001.html");
+    $("#mrhMovimentacaoHorizontal").load("section_001.html");
+    $("#mrhMovimentacaoVertical").load("section_001.html");
+});
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+        $(".navHide").addClass("navShow");
+    } else {
+        $(".navHide").removeClass("navShow");
+    }
+});
+$(document).ready(function() {
+    $("body").tooltip({ selector: '[data-bs-toggle=tooltip]' });
+});
+function showDiv(select) {
+    if (select.value==7) {
+        document.getElementById('hiddenMotivoOutros').style.display = "flex";
+    } else{
+        document.getElementById('hiddenMotivoOutros').style.display = "none";
+    }
+ }
