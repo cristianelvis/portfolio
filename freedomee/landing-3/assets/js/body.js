b6ac6f2c314e75372cc371cdbc52995d2445a76c
Cristian Elvis <79227065+cristianelvis@users.noreply.github.com>
2022-06-01 21:03:25 -0300

    Update body.js

--- freedomee/landing-3/assets/js/body.js
+++ freedomee/landing-3/assets/js/body.js
@@ -29,6 +29,36 @@ $(document).ready(function () {
     $('#card-quality').mouseleave(function(){
         $('#content-quality').addClass('closed').removeClass('open');
     });
+    $('#card-encourage').mouseenter(function(){
+        $('#content-encourage').addClass('open').removeClass('closed');
+    });
+    $('#card-encourage').mouseleave(function(){
+        $('#content-encourage').addClass('closed').removeClass('open');
+    });
+    $('#card-multiply').mouseenter(function(){
+        $('#content-multiply').addClass('open').removeClass('closed');
+    });
+    $('#card-multiply').mouseleave(function(){
+        $('#content-multiply').addClass('closed').removeClass('open');
+    });
+    $('#card-humanize').mouseenter(function(){
+        $('#content-humanize').addClass('open').removeClass('closed');
+    });
+    $('#card-humanize').mouseleave(function(){
+        $('#content-humanize').addClass('closed').removeClass('open');
+    });
+    $('#card-empower').mouseenter(function(){
+        $('#content-empower').addClass('open').removeClass('closed');
+    });
+    $('#card-empower').mouseleave(function(){
+        $('#content-empower').addClass('closed').removeClass('open');
+    });
+    $('#card-thrive').mouseenter(function(){
+        $('#content-thrive').addClass('open').removeClass('closed');
+    });
+    $('#card-thrive').mouseleave(function(){
+        $('#content-thrive').addClass('closed').removeClass('open');
+    });
     $(function () {
         $('[data-bs-toggle="popover"]').popover({ trigger: "hover" })
     })
