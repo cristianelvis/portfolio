47755689ad11fa1df34f5855bf03b1e5553e9e43
Cristian Elvis <79227065+cristianelvis@users.noreply.github.com>
2022-06-23 09:13:37 -0300

    Add files via upload

--- firjan/marisa/assets/js/scripts.js
+++ firjan/marisa/assets/js/scripts.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
     $("#footer").load("../dashboard/_include_footer.html");
     $("#modal").load("../dashboard/_include_modal.html");
     $("#dashboard").load("section_001.html");
-    $("#gestaodepessoas").load("section_001.html");
+    $("#gestaoDePessoasNovasSolicitacoes").load("section_001.html");
+    $("#gestaoDePessoasConsultarSolicitacoes").load("section_001.html");
+    $("#tarefasAConcluir").load("section_001.html");    
     $("#mrhDesligamento").load("section_001.html");
     $("#mrhAdmissao").load("section_001.html");
     $("#mrhAdmissaoSubstituicao").load("section_001.html");
@@ -29,5 +31,12 @@ function showDiv(select) {
         document.getElementById('hiddenMotivoOutros').style.display = "flex";
     } else{
         document.getElementById('hiddenMotivoOutros').style.display = "none";
+    }
+ };
+ function showDiv2(select) {
+    if (select.value==4) {
+        document.getElementById('hiddenResponsabilidade').style.display = "flex";
+    } else{
+        document.getElementById('hiddenResponsabilidade').style.display = "none";
     }
  }
