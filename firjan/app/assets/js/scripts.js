4f0df676bbf073575c6da6db34c5a416c799f3b8
Cristian Elvis <79227065+cristianelvis@users.noreply.github.com>
2022-07-04 11:30:04 -0300

    Add files via upload

--- firjan/app/assets/js/scripts.js
+++ firjan/app/assets/js/scripts.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
     $("#mrhAdmissaoSubstituicao").load("section_001.html");
     $("#mrhAlteracaoCargaHoraria").load("section_001.html");
     $("#mrhAlteracaoCargoEspecialidade").load("section_001.html");
+    $("#mrhAlteracaoItemContabilCentroCusto").load("section_001.html");
     $("#mrhMovimentacaoHorizontal").load("section_001.html");
     $("#mrhMovimentacaoVertical").load("section_001.html");
     $("#mrhTransferencia").load("section_001.html");
@@ -32,11 +33,28 @@ function showDiv(select) {
     } else{
         document.getElementById('hiddenMotivoOutros').style.display = "none";
     }
- };
- function showDiv2(select) {
+};
+function showDiv2(select) {
     if (select.value==4) {
         document.getElementById('hiddenResponsabilidade').style.display = "flex";
     } else{
         document.getElementById('hiddenResponsabilidade').style.display = "none";
     }
- }
+};
+function showDiv3(select) {
+    if (select.value==1) {
+        document.getElementById('lineItemContabil').style.display = "flex";
+    } else{
+        document.getElementById('lineItemContabil').style.display = "none";
+    };
+    if (select.value==2) {
+        document.getElementById('lineCentroCusto').style.display = "flex";
+    } else{
+        document.getElementById('lineCentroCusto').style.display = "none";
+    };
+    if (select.value==3) {
+        document.getElementById('lineAmbos').style.display = "flex";
+    } else{
+        document.getElementById('lineAmbos').style.display = "none";
+    }
+}
